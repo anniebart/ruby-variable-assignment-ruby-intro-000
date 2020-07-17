c168846025dc816e0fbe14d479a98bc84032e64f
Annie Bartholomew <anniebartholomew@gmail.com>
2020-07-17 00:17:41 +0000

    Automatically backed up by Learn

--- spec/variable_spec.rb
+++ spec/variable_spec.rb
@@ -1,7 +1,9 @@
 # Code your solution in variable.rb
 describe "./variable.rb" do
 
-  greeting = "Hello World"
+  it "defined a local variable called greeting and set it equal to 'Hello World'" do
+    greeting = "Hello World"
+
     expect(greeting).to eq("Hello World")
   end
 
